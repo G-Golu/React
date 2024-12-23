0e63a626e8dec50220fb36fa8b7b5659e9cc2135
Golu Kumar <golug73700@gmail.com>
2024-12-23 14:43:24 +0530

    first commit

---
+++ Second project/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <h1>Hello Duniya!</h1>
+    <p>hey</p>
+    <body>
+      hello World!
+    </body>
+    
+  </StrictMode>,
+)
